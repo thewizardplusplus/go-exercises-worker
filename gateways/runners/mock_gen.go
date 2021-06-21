471331ca078bc0e128f89ae00616c7142826c53e
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-21 23:00:00 +0300

    Generate a mock for the log.Logger interface for the runners package

---
+++ gateways/runners/mock_gen.go
@@ -0,0 +1,15 @@
+package runners
+
+import (
+	"github.com/go-log/log"
+)
+
+//go:generate mockery --name=Logger --inpackage --case=underscore --testonly
+
+// Logger ...
+//
+// It is used only for mock generating.
+//
+type Logger interface {
+	log.Logger
+}
