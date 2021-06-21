2fea5dc66e02f88c449bbe27aa9c681da77d632a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-21 23:00:00 +0300

    Add basics of tests for the queues.SolutionHandler.HandleMessage() method

--- gateways/queues/solution_result_handler_test.go
+++ gateways/queues/solution_result_handler_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/thewizardplusplus/go-exercises-worker/entities"
 )
 
@@ -14,4 +15,40 @@ func TestSolutionHandler_MessageType(test *testing.T) {
 
 	wantedMessageType := reflect.TypeOf(entities.Solution{})
 	assert.Equal(test, wantedMessageType, receivedMessageType)
+}
+
+func TestSolutionHandler_HandleMessage(test *testing.T) {
+	type fields struct {
+		SolutionResultQueueName string
+		SolutionRunner          SolutionRunner
+		MessagePublisher        MessagePublisher
+	}
+	type args struct {
+		message interface{}
+	}
+
+	for _, data := range []struct {
+		name      string
+		fields    fields
+		args      args
+		wantedErr assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			handler := SolutionHandler{
+				SolutionResultQueueName: data.fields.SolutionResultQueueName,
+				SolutionRunner:          data.fields.SolutionRunner,
+				MessagePublisher:        data.fields.MessagePublisher,
+			}
+			receivedErr := handler.HandleMessage(data.args.message)
+
+			mock.AssertExpectationsForObjects(
+				test,
+				data.fields.SolutionRunner,
+				data.fields.MessagePublisher,
+			)
+			data.wantedErr(test, receivedErr)
+		})
+	}
 }
