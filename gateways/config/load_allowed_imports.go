f0cc1dc27092ee2f5362d55edb94533234fc5aa3
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-26 23:00:00 +0300

    Add the config.LoadAllowedImports() function

---
+++ gateways/config/load_allowed_imports.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	mapset "github.com/deckarep/golang-set"
+	"github.com/pkg/errors"
+)
+
+// LoadAllowedImports ...
+func LoadAllowedImports(configPath string) (mapset.Set, error) {
+	allowedImportsAsJSON, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read the allowed imports")
+	}
+
+	var allowedImportAsSlice []string
+	err = json.Unmarshal(allowedImportsAsJSON, &allowedImportAsSlice)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to unmarshal the allowed imports")
+	}
+
+	allowedImports := mapset.NewSet()
+	for _, allowedImport := range allowedImportAsSlice {
+		allowedImports.Add(allowedImport)
+	}
+
+	return allowedImports, nil
+}
