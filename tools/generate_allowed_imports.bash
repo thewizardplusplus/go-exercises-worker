5cf9da030a36c8f91894fbdd9aa3189a0a04e76b
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-26 23:00:00 +0300

    Add the wrap_lines_with_brackets() function to the script for generating allowed imports

--- tools/generate_allowed_imports.bash
+++ tools/generate_allowed_imports.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+function wrap_lines_with_brackets() {
+  declare -r left_bracket="$1"
+  declare -r right_bracket="$2"
+
+  sed --regexp-extended "s/(.*)/$left_bracket\1$right_bracket/"
+}
+
 declare -ra disabledImports=(
   "builtin"
   "cmd"
@@ -10,6 +17,6 @@ go list "$(go env GOROOT)/src/..." \
   | grep \
     --invert-match \
     --perl-regexp "$(IFS="|"; echo "${disabledImports[*]}")" \
-  | sed --regexp-extended 's/(.*)/"\1"/' \
+  | wrap_lines_with_brackets '"' '"' \
   | paste --serial --delimiters ',' \
-  | sed --regexp-extended 's/(.*)/[\1]/'
+  | wrap_lines_with_brackets "[" "]"
