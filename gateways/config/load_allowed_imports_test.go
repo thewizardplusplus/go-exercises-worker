6cdfbc0815bb65bac92506b53ef265a3e49c260a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-26 23:00:00 +0300

    Implement tests of the config.LoadAllowedImports() function

--- gateways/config/load_allowed_imports_test.go
+++ gateways/config/load_allowed_imports_test.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	mapset "github.com/deckarep/golang-set"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLoadAllowedImports(test *testing.T) {
@@ -14,10 +17,55 @@ func TestLoadAllowedImports(test *testing.T) {
 		wantAllowedImports mapset.Set
 		wantedErr          assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success without duplicates",
+			prepareConfig: func(test *testing.T) (configPath string) {
+				config, err := ioutil.TempFile("", "allowed_imports")
+				require.NoError(test, err)
+				defer config.Close()
+
+				_, err = config.Write([]byte(`["one", "two"]`))
+				require.NoError(test, err)
+
+				return config.Name()
+			},
+			wantAllowedImports: mapset.NewSet("one", "two"),
+			wantedErr:          assert.NoError,
+		},
+		{
+			name: "success with duplicates",
+			prepareConfig: func(test *testing.T) (configPath string) {
+				config, err := ioutil.TempFile("", "allowed_imports")
+				require.NoError(test, err)
+				defer config.Close()
+
+				_, err = config.Write([]byte(`["one", "two", "two"]`))
+				require.NoError(test, err)
+
+				return config.Name()
+			},
+			wantAllowedImports: mapset.NewSet("one", "two"),
+			wantedErr:          assert.NoError,
+		},
+		{
+			name: "error with unmarshalling",
+			prepareConfig: func(test *testing.T) (configPath string) {
+				config, err := ioutil.TempFile("", "allowed_imports")
+				require.NoError(test, err)
+				defer config.Close()
+
+				_, err = config.Write([]byte("incorrect"))
+				require.NoError(test, err)
+
+				return config.Name()
+			},
+			wantAllowedImports: nil,
+			wantedErr:          assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			configPath := data.prepareConfig(test)
+			defer os.Remove(configPath)
 
 			receivedAllowedImports, receivedErr := LoadAllowedImports(configPath)
 
